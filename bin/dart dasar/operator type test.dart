87c16dcd6bc2b3d50b8c8afc49b8cd25a6a88133
daywsenada <daywsenadaart@gmail.com>
2024-10-22 05:48:51 +0800

    2

---
+++ bin/dart dasar/operator type test.dart
@@ -0,0 +1,11 @@
+void main(){
+
+//operator type test
+  dynamic variable = 100;
+
+  var variableInt = variable as int;
+
+  var isInt = variable is int;
+  var isNotBoolean = variable is! bool;
+  
+}
